d3eccc7f9304b8b848b9cc839a22a0d5cc306007
Maciek <maciek1839@gmail.com>
2024-04-24 09:50:14 +0200

    chore(release): set development version to 0.0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 
 allprojects{
     group = "com.showmeyourcode.playground"
-    version = "0.0.1"
+    version = "0.0.2-SNAPSHOT"
 
     apply(plugin ="java")
     apply(plugin ="org.sonarqube")
